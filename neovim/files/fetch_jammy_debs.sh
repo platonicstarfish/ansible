dbe129d4b40b1a53dfaa2ffdbf7f32c76ab9a7dc
platonicstarfish <platonicstarfish@protonmail.com>
2022-05-24 14:30:54 +0200

    Neovim is now a role

---
+++ neovim/files/fetch_jammy_debs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/l/luajit/libluajit-5.1-common_2.1.0%7ebeta3%2bdfsg-6_all.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/l/luajit/libluajit-5.1-2_2.1.0%7ebeta3%2bdfsg-6_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/t/tree-sitter/libtree-sitter0_0.20.3-1_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/u/unibilium/libunibilium4_2.1.0-1_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/l/lua-luv/lua-luv_1.36.0-0-1_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/main/p/python-msgpack/python3-msgpack_1.0.3-1build1_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/main/p/python-greenlet/python3-greenlet_1.1.2-3build1_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/p/python-pynvim/python3-pynvim_0.4.2-1_all.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/p/python-pynvim/python3-neovim_0.4.2-1_all.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/x/xclip/xclip_0.13-2_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/m/msgpack-c/libmsgpackc2_3.3.0-4_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/libt/libtermkey/libtermkey1_0.22-1_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/libv/libvterm/libvterm0_0.1.4-1_amd64.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/n/neovim/neovim-runtime_0.6.1-3_all.deb
+wget -c http://dk.archive.ubuntu.com/ubuntu/pool/universe/n/neovim/neovim_0.6.1-3_amd64.deb
+
