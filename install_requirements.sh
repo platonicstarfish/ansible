dffe1716cdd6e463938b6fd158f5410cf86a9d1a
platonicstarfish <platonicstarfish@protonmail.com>
2022-05-27 23:11:48 +0000

    root-to-user became a role

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 ansible-galaxy install staticdev.pyenv
+ansible-galaxy install kwoodson.yedit
 ansible-galaxy collection install community.general
+pip3 install petname
+pip3 install stormssh
+apt install pwgen
+
