c9aa0868bbb0d7c5b2ce55c1e5b501cf98971875
platonicstarfish <platonicstarfish@protonmail.com>
2023-03-12 11:29:11 +0100

    VS Code install in devbox

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,6 +2,8 @@
 ansible-galaxy install staticdev.pyenv
 ansible-galaxy install kwoodson.yedit
 ansible-galaxy collection install community.general
+ansible-galaxy install gantsign.visual-studio-code
+ansible-galaxy install gantsign.visual-studio-code-extensions
 pip3 install petname
 pip3 install stormssh
 apt install pwgen
